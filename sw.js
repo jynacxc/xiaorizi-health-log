ab84f560101ad5d6e7714ff70b6bb6403b59de18
jynacxc <564188050@qq.com>
2026-09-22 12:02:31 +0800

    刷新小日子应用缓存版本

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'little-days-v2';
+const CACHE_NAME = 'little-days-v3';
 const APP_ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
@@ -23,3 +23,4 @@ self.addEventListener('fetch', (event) => {
     }).catch(() => caches.match('./index.html')))
   );
 });
+
